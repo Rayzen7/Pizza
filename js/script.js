d45895dcfc3eaf16fc097f7e6bb57d257ed23c40
Setio <nugrahasetio6@gmail.com>
2023-09-23 09:54:25 +0700

    Pizza Mood

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Toogle class active
+const navbarNav = document.querySelector(".navbar-nav");
+// Ketika hamburger menu di kilik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Klik diluar slidebar untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
